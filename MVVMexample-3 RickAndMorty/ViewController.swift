e262dead2eaaca27ef9424bb7873616f8f33aaa8
altintopa <83283552+altintopa@users.noreply.github.com>
2022-05-18 23:49:44 +0300

    FirstOne

---
+++ MVVMexample-3 RickAndMorty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVMexample-3 RickAndMorty
+//
+//  Created by Ahmet WOW on 18/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var sgmType: UISegmentedControl!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
---
+++ MVVMexample-3 RickAndMorty/collectionCell.swift
@@ -0,0 +1,17 @@
+//
+//  collectionCell.swift
+//  MVVMexample-3 RickAndMorty
+//
+//  Created by Ahmet WOW on 18/05/2022.
+//
+
+import UIKit
+
+class collectionCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
