870d571c65beb94f6014ca22f09b041451ddf992
altintopa <83283552+altintopa@users.noreply.github.com>
2022-05-19 16:51:51 +0300

    addedModel

---
+++ MVVMexample-3 RickAndMorty/Models.swift
@@ -0,0 +1,46 @@
+//
+//  Models.swift
+//  MVVMexample-3 RickAndMorty
+//
+//  Created by Ahmet WOW on 19/05/2022.
+//
+
+import Foundation
+
+
+struct RickAndMortyModel: Codable {
+    
+    let name: String
+    let status: Status
+    let species: Species
+    let type: String
+    let gender: Gender
+    let origin, location: Location
+    let image: String
+    let episode: [String]
+    let url: String
+    let created: String
+}
+
+enum Gender: String, Codable {
+    case female = "Female"
+    case male = "Male"
+    case unknown = "unknown"
+}
+
+enum Species: String, Codable {
+    case alien = "Alien"
+    case human = "Human"
+}
+
+enum Status: String, Codable {
+    case alive = "Alive"
+    case dead = "Dead"
+    case unknown = "unknown"
+}
+
+// MARK: - Location
+struct Location: Codable {
+    let name: String
+    let url: String
+}
